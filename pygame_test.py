c1fe20c8f0bf237ff7f1eb2c6bc4bff570b7df0c
syki66 <66syki@gmail.com>
2020-04-21 23:19:42 +0900

    add a number of enemy

--- pygame_test.py
+++ pygame_test.py
@@ -13,25 +13,30 @@ win = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("game test")
 
 # 플레이어
-x = 50
-y = 50
-width = 20
-height = 20
-vel = 5
+x = 50 # 플레이어 초기 x 위치값
+y = 50 # 플레이어 초기 y 위치값
+width = 20 # 플레이어 높이
+height = 20 # 플레이어 너비
+vel = 5 # 이동속도
 
 # 적
-x1 = random.randrange(100,screenHeight)
-y1 = random.randrange(100,screenWidth) # 랜덤한 위치에서 생성
+enemyNum = 20 # 적 개수
 
+# 적 위치 값을 배열 넣기, 랜덤한 초기 위치값으로 설정
+enemy = []
+for i in range(enemyNum):
+    enemy.append([random.randrange(100,screenHeight), random.randrange(100,screenWidth)]) # 
 
 run = True
 while run:
-    pygame.time.delay(15)
+    pygame.time.delay(15) # 게임 속도조절
 
+    # 윈도우창 끌수 있게끔 하는거
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    # 키 눌렸을때 설정하는것, 화면 밖으로 못나가게끔 설정함
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT] and x > vel:
@@ -45,34 +50,31 @@ while run:
     if keys[pygame.K_SPACE]:
        isJump = True
             
-    win.fill((0,0,0))
-    pygame.draw.rect(win, (255,255, 255), (x, y, width, height))
-
-
-
-    # 적 움직임
-    pygame.draw.rect(win, (255, 0, 0), (x1, y1, width, height))
-    rand = random.randrange(0,4)
-
-    if rand == 1 and x1 > vel:
-        x1 -= vel
-    if rand == 2 and x1 < screenWidth - width - vel:
-        x1 += vel
-    if rand == 3 and y1 > vel:
-        y1 -= vel
-    if rand == 4 and y1 < screenHeight - height - vel:
-        y1 += vel
-
-    distance = ( (x1 - x)**2 + (y1 - y)**2 ) ** 0.5
+    win.fill((0,0,0)) # 각 도형 자취 안남도록 하는것 및 배경설정
     
-    if (distance < 20):
-        print("game over")
-        run = False
+    pygame.draw.rect(win, (255,255, 255), (x, y, width, height)) # 플레이어 그리기
 
 
+    # 적 움직임, 우선 랜덤하게 각자 움직이게함
+    for i in range(len(enemy)):
+        pygame.draw.rect(win, (255, 0, 0), (enemy[i][0], enemy[i][1], width, height))
+        randomMove = random.randrange(0,4)
 
+        if randomMove == 0 and enemy[i][0] > vel:
+            enemy[i][0] -= vel
+        if randomMove == 1 and enemy[i][0] < screenWidth - width - vel:
+            enemy[i][0] += vel
+        if randomMove == 2 and enemy[i][1] > vel:
+            enemy[i][1] -= vel
+        if randomMove == 3 and enemy[i][1] < screenHeight - height - vel:
+            enemy[i][1] += vel
 
-    
+        # 거리가 20 이하로 줄어들면 게임 오버 시킴
+        distance = ( (enemy[i][0] - x)**2 + (enemy[i][1] - y)**2 ) ** 0.5 
+        if (distance < 20):
+            print("game over")
+            run = False
+
     pygame.display.update()
 
 pygame.quit()
